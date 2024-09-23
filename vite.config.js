b06b19af62b5f17a635af2c9248849b4cc796831
Robin <97387960+robeecodes@users.noreply.github.com>
2024-09-23 11:27:31 +0100

    fix: fixed path to apse-designs

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
 import restart from 'vite-plugin-restart'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-    },
+        {
+            host: true, // Open to local network and display URL
+            open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        },
     build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
+        {
+            rollupOptions: {
+                input: {
+                    main: '/index.html',
+                    apseDesigns: '/apse-designs.html',
+                },
+            },
+            outDir: '../dist', // Output in the dist/ folder
+            emptyOutDir: true, // Empty the folder first
+            sourcemap: true // Add sourcemap
+        },
     plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
-}
+        [
+            restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+        ],
+})
