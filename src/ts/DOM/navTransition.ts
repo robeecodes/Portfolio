029bd734e0a91148dbe0fe64a4542424488e477a
Robin <97387960+robeecodes@users.noreply.github.com>
2024-10-18 10:54:15 +0100

    fix(src/ts/DOM/navTransition.ts): fix scroll snapping glitch when user has mousedown

--- src/ts/DOM/navTransition.ts
+++ src/ts/DOM/navTransition.ts
@@ -12,13 +12,13 @@ const transitionalElems: NodeListOf<Element> | null = document.querySelectorAll(
 const transition: Function = () => {
     transitionalElems.forEach((el: Element) => {
         if (navBurger?.getAttribute('aria-expanded') === 'true') {
-            gsap.to (el, {
+            gsap.to(el, {
                     duration: 0.2,
                     transform: 'translateX(-100%)',
                 }
             )
         } else {
-            gsap.to (el, {
+            gsap.to(el, {
                     duration: 0.2,
                     transform: 'translateX(0)',
                 }
@@ -28,13 +28,13 @@ const transition: Function = () => {
 }
 
 document.addEventListener('click', (e) => {
-   let target = e.target as Element;
-   if (!navBar?.contains(target) && !navBurger?.contains(target) && navBurger?.getAttribute('aria-expanded') === 'true') {
-       if (target !== navBar) {
-           navBurger?.setAttribute('aria-expanded', 'false');
-           transition();
-       }
-   }
+    let target = e.target as Element;
+    if (!navBar?.contains(target) && !navBurger?.contains(target) && navBurger?.getAttribute('aria-expanded') === 'true') {
+        if (target !== navBar) {
+            navBurger?.setAttribute('aria-expanded', 'false');
+            transition();
+        }
+    }
 });
 
 navBurger?.addEventListener('click', () => {
@@ -61,16 +61,26 @@ const updateLink = (link: HTMLAnchorElement) => {
 let snappingDisabled: boolean = false;
 let targetSection: string | null = null;
 
-navLinks.forEach((navLink: HTMLAnchorElement) => {
-   navLink.addEventListener('click', () => {
-       updateLink(navLink);
-       navExit?.click();
-       transition();
+// Add event listener for detecting the start of scrollbar dragging
+window.addEventListener('mousedown', () => {
+    snappingDisabled = true;
+});
 
-       snappingDisabled = true;
-       // @ts-ignore
-       targetSection = navLink.getAttribute('href').substring(1);
-   });
+// Add event listener for detecting when the scrollbar dragging ends
+window.addEventListener('mouseup', () => {
+    snappingDisabled = false;
+});
+
+navLinks.forEach((navLink: HTMLAnchorElement) => {
+    navLink.addEventListener('click', () => {
+        updateLink(navLink);
+        navExit?.click();
+        transition();
+
+        snappingDisabled = true;
+        // @ts-ignore
+        targetSection = navLink.getAttribute('href').substring(1);
+    });
 });
 
 let contactBtn: HTMLAnchorElement | null = document.querySelector('#hero a');
@@ -90,7 +100,7 @@ const observer = new IntersectionObserver(observerCallback, observerOptions);
 
 function observerCallback(entries: any, _observer: any) {
     entries.forEach((entry: any) => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             if (entry.target.id === 'hero') {
                 updateLink(navLinks[0]);
                 if (!snappingDisabled) scrollSnapping(entry.target);
